17b46e7a0368d2fa565108b38224ee0e93b5d303
Armando De Santiago <armando.desantiago6265@alumnos.udg.mx>
2024-11-26 20:57:35 -0600

    Funcion a "ver mas" (No comprobado)

--- publicacion/urls.py
+++ publicacion/urls.py
@@ -5,4 +5,5 @@ app_name = 'publicacion'
 urlpatterns = [
     path('profesor/<int:profesor_id>', views.ProfesorView.as_view(), name='profesor'),
     path('materia/<int:materia_id>', views.MateriaView.as_view(), name='materia'),
+    path('profesor/<int>detalle_publicacion_id>', views.DetallePublicacionView.as_view(), name='detalle'),
 ]
